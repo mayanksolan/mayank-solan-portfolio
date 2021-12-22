493ac1a97186a3145e09f40a9ca8395d6f66cf13
Mayank Solan <mayank@giveindia.org>
2021-12-22 14:23:42 +0530

    call changes

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
 
   getResumeData() {
     $.ajax({
-      url: "/mayank-solan-portfolio/resumeData.json",
+      url: "/resumeData.json",
       dataType: "json",
       cache: false,
       success: function (data) {
